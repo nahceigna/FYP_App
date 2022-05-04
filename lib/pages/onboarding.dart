09c77d3097106e5211f66a6b3b784141b9d3482e
nahceigna <angiechan0104@gmail.com>
2022-05-04 15:54:28 +0800

    upload demo photo

--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -17,50 +17,53 @@ class _OnBoardingState extends State<OnBoarding> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Image(image: AssetImage('images/onboarding.png')),
-              const Text(
-                "Welcome to Finder",
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF03045E),
-                ),
-              ),
-              const SizedBox(height: 10),
-              const Text(
-                "Get real-time occupancy information of the HKUST library to help you plan ahead!",
-                style: TextStyle(
-                  fontSize: 17,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 40),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15)),
-                  primary: Colors.blue[600],
-                ),
-                onPressed: () async {
-                  final reference = await SharedPreferences.getInstance();
-                  reference.setBool(
-                      'showHome', true); // will be stored in the phone
-                  Navigator.of(context).pushReplacementNamed('/loading');
-                },
-                child: const Text(
-                  "Get Started",
+          child: Center(
+            heightFactor: 1.4,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const Image(image: AssetImage('images/onboarding.png')),
+                const Text(
+                  "Welcome to Finder",
                   style: TextStyle(
-                    fontSize: 20,
+                    fontSize: 25,
                     fontWeight: FontWeight.bold,
+                    color: Color(0xFF03045E),
                   ),
                 ),
-              ),
-            ],
+                const SizedBox(height: 10),
+                const Text(
+                  "Get real-time occupancy information of the HKUST library to help you plan ahead!",
+                  style: TextStyle(
+                    fontSize: 17,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 40),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 40, vertical: 15),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15)),
+                    primary: Colors.blue[600],
+                  ),
+                  onPressed: () async {
+                    final reference = await SharedPreferences.getInstance();
+                    reference.setBool(
+                        'showHome', true); // will be stored in the phone
+                    Navigator.of(context).pushReplacementNamed('/loading');
+                  },
+                  child: const Text(
+                    "Get Started",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
